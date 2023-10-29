421491ba5c7c57905318ede933a6e93274911e69
a-mystic <totocaca@naver.com>
2023-10-29 09:31:13 +0900

    add iOS project.

---
+++ Term Project/DBTermProject/DBTermProject/DBTermProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBTermProjectApp.swift
+//  DBTermProject
+//
+//  Created by a mystic on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DBTermProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
