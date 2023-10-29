c6181b8642e8d51ce5fc152c02e2f3e7a057d212
a-mystic <totocaca@naver.com>
2023-10-29 10:15:12 +0900

    add draggable UI & modify UI.

--- Term Project/DBTermProject/DBTermProject/ContentView.swift
+++ Term Project/DBTermProject/DBTermProject/ContentView.swift
@@ -9,24 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.gray
-                .opacity(0.1)
-                .ignoresSafeArea(edges: .bottom)
-            VStack {
-                Spacer()
-                dropBox
-                Spacer()
-                sqlSlider
+        NavigationStack {
+            ZStack {
+                Color.gray
+                    .opacity(0.1)
+                    .ignoresSafeArea(edges: .bottom)
+                VStack(spacing: 30) {
+                    dropBox
+                    Spacer()
+                }
+                .frame(height: 500)
+                VStack {
+                    Spacer()
+                    sqlSlider
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    run
+                }
             }
         }
     }
     
     private var dropBox: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 14)
-                .foregroundStyle(.black.opacity(0.5))
-//                .frame(width: 300, height: 300)
+            Color.black
+                .opacity(0.5)
+                .ignoresSafeArea(edges: .bottom)
             VStack(spacing: 30) {
                 Image(systemName: "rectangle.and.hand.point.up.left.filled")
                     .imageScale(.large)
@@ -42,37 +53,65 @@ struct ContentView: View {
     }
     
     private func drop(_ items: [String]) -> Bool {
+        if let item = items.first {
+            selectedSqlCommands.append(item)
+            commands.append("")
+        }
         return true
     }
     
-    @State private var commands: [String] = [""]
+    @State private var commands: [String] = []
+    @State private var selectedSqlCommands: [String] = []
     
     private var droppedCommandsList: some View {
         VStack(spacing: 10) {
             Spacer()
                 .frame(height: 10)
-            HStack {
-                Text("Select")
-                TextField("SQL명령어를 입력해주세요", text: $commands[0])
-                    .textFieldStyle(.roundedBorder)
+            ForEach(selectedSqlCommands.indices, id: \.self) { index in
+                HStack {
+                    Text(selectedSqlCommands[index]).bold()
+                    TextField("SQL명령어를 입력해주세요", text: $commands[index])
+                        .textFieldStyle(.roundedBorder)
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
             Spacer()
         }
     }
     
-    private let sqlCommands = ["Select", "Insert", "From", "Where", "Order by", "Join"]
+    private var run: some View {
+        Button {
+            running()
+        } label: {
+            Image(systemName: "arrowtriangle.right.fill")
+        }
+    }
+    
+    private func running() {
+        var command = ""
+        for index in selectedSqlCommands.indices {
+            command += selectedSqlCommands[index] + " " + commands[index] + " "
+        }
+        print(command)
+    }
+    
+    private let sqlCommands = ["Select", "Insert", "From", "Where", "Order by", "Join", "Into"]
     
     private var sqlSlider: some View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(sqlCommands, id: \.self) { sql in
-                    Text(sql)
-                        .font(.system(size: 15))
-                        .padding()
-                        .border(.black)
-                        .clipShape(RoundedRectangle(cornerRadius: 3))
-                        .draggable(sql)
+                    ZStack {
+                        Text(sql)
+                            .foregroundStyle(.white)
+                            .font(.system(size: 15))
+                            .padding()
+                            .background {
+                                RoundedRectangle(cornerRadius: 14)
+                                    .fill(.blue.opacity(0.9))
+                            }
+                    }
+                    .draggable(sql)
                 }
             }
             .padding(.horizontal)
