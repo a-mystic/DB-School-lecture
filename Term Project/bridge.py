32f619dfacd0f6ca515accfc17af9ce46e1040b9
a-mystic <totocaca@naver.com>
2023-11-01 09:41:12 +0900

    update termproject backend

--- Term Project/bridge.py
+++ Term Project/bridge.py
@@ -1,15 +1,41 @@
 from fastapi import FastAPI
+import pymysql
 
+# initialize
 app = FastAPI()
+connect = pymysql.connect(
+    host='192.168.56.101',
+    port=4567,
+    user='dohyunkim',
+    password='0000',
+    db='Termproject',
+    charset='utf8'
+)
+cursor = connect.cursor()
 
 @app.get("/select")
 async def select(command: str):
-    return ""
+    cursor.execute(command)
+    returnValue = ""
+    results = cursor.fetchall()
+    for result in results:
+        returnValue += result
+    disappear()
+    return returnValue
 
 @app.get("/insert")
 async def insert(command: str):
-    return ""
+    cursor.execute(command)
+    disappear()
+    return "입력하신 데이터가 입력되었습니다."
 
 @app.get("/delete")
 async def delete(command: str):
-    return ""
+    cursor.execute(command)
+    disappear()
+    return "입력하신 데이터가 삭제되었습니다."
+
+def disappear():
+    connect.commit()
+    cursor.close()
+    connect.close()
